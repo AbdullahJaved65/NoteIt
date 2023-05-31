073f772c668585c78236e6d34625243deac96308
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-05-31 14:02:42 +0500

    - Remove field status from task - Add field creator to note - Add field completed to task - Add field creator to task - Added login and registration pages - Added a temp db - Added multiple pages

--- base/models.py
+++ base/models.py
@@ -1,31 +1,37 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField(max_length=1000)
+    creator = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                default=None)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-updated', '-created']
-        
-        
+
     def __str__(self):
         return self.title
-    
-    
+
+
 class Task(models.Model):
     title = models.CharField(max_length=300)
-    status = models.CharField(max_length=100)
+    completed = models.BooleanField(default=False)
+    creator = models.ForeignKey(User,
+                                on_delete=models.CASCADE,
+                                null=True,
+                                default=None)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
-    
+
     class Meta:
         ordering = ['-updated', '-created']
-        
-        
+
     def __str__(self):
         return self.title
-    
--- base/views.py
+++ base/views.py
@@ -14,6 +14,56 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+            user_db = User.objects.get(username='username')
+        except User.DoesNotExist:
+            messages.error(request, 'The User does not exist!')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR Password is invalid')
+
+    context = {'page': page}
+    return render(request, 'base/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    redirect('home')
+
+
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            redirect('home')
+        else:
+            messages.error(request, 'Something went wrong')
+
+    context = {'page': page, 'form': form}
+    return render(request, 'base/register.html', context)
+
+
 def home(request):
     context = {}
     return render(request, 'base/home.html', context)
